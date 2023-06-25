d082f168580fccc1bc1e349992122af2faf01177
Sangmin <101931428+netco97@users.noreply.github.com>
2023-06-25 21:09:10 +0900

    Initial commit

---
+++ src/test/java/com/example/ajax_test/AjaxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajax_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
