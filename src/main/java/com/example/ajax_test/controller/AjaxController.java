e972b029d461cb110ce71b3c20dfd8411e24d9d1
Sangmin <101931428+netco97@users.noreply.github.com>
2023-06-28 20:25:57 +0900

    ajax get/post request

--- src/main/java/com/example/ajax_test/controller/AjaxController.java
+++ src/main/java/com/example/ajax_test/controller/AjaxController.java
@@ -2,12 +2,37 @@ package com.example.ajax_test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxController {
     @GetMapping("/ex01")
     public String ex01(){
         System.out.println("AjaxController.ex01");
-        return "index";
+        return "index"; //index.html에 작성된 내용
+    }
+
+    @PostMapping("/ex02")
+    public @ResponseBody String ex02(){ //Http프로토콜의 Body를 의미
+        System.out.println("AjaxController.ex02");
+        return "안녕하세요"; // 리턴값이 그대로 출력됨.
+    }
+
+    @GetMapping("/ex03")
+    public @ResponseBody String ex03(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        return "ex3 호출 완료";
+
+    }
+
+    @PostMapping("/ex04")
+    public @ResponseBody String ex04(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        return "ex3 호출 완료";
+
     }
 }
--- src/main/java/com/example/ajax_test/controller/AjaxViewController.java
+++ src/main/java/com/example/ajax_test/controller/AjaxViewController.java
@@ -9,4 +9,19 @@ public class AjaxViewController {
     public String ajaxEx01(){
         return "ajax-ex-01";
     }
+
+    @GetMapping("/ajax-ex-02")
+    public String ajaxEx02(){
+        return "ajax-ex-02";
+    }
+
+    @GetMapping("/ajax-ex-03")
+    public String ajaxEx03(){
+        return "ajax-ex-03";
+    }
+
+    @GetMapping("/ajax-ex-04")
+    public String ajaxEx04(){
+        return "ajax-ex-04";
+    }
 }
